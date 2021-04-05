4d495bf8d7fe38b939edf0b931ac068c6600b3bd
Leonardo Oliveira <leonfoliveira@gmail.com>
2021-04-04 22:13:13 -0300

    feat: add Root component

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import Root from './main/root';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <span />
+    <Root />
   </React.StrictMode>,
   document.getElementById('root'),
 );
---
+++ src/main/root.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import Dashboard from '@/presentation/dashboard/dashboard';
+
+import '@/presentation/styles/global.scss';
+
+const Root: React.FC = () => <Dashboard />;
+
+export default Root;
