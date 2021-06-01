4937ee7529c7bebd0008c0dc713ce7a26b1cb6bb
Leonardo Oliveira <leonfoliveira@gmail.com>
2021-06-01 17:36:25 -0300

    chore: increase default window size

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 function createWindow() {
   const win = new BrowserWindow({
-    width: 800,
+    width: 1000,
     height: 600,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
